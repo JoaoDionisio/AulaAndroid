4f1c0dacb114d6193ddb7405a83971fd3cbe7c1b
JoaoDionisio <jd1583@hotmail.com>
2017-11-15 21:00:52 -0200

    Update MainActivity.java

--- app/src/main/java/com/example/goddionisio/aula02atividadepdm/MainActivity.java
+++ app/src/main/java/com/example/goddionisio/aula02atividadepdm/MainActivity.java
@@ -18,7 +18,7 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     //tentando adicionar o git
     //COMENTARIO ADD PELO SITE GITHUB
-    //Minha ADD local
+    //Minha alteracao pelo site!
 
 
     private EditText valorEdt01, valorEdt02;
